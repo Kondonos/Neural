9ef5d2695fa0aadb60544047ba872ea60649bf15
Max Henschell <max.Henschell@cargoguard.com>
2018-02-25 20:31:15 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Neural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="Neural network";
+
+            var config=new  ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json")
+                .Build();
+
+            var services=new ServiceCollection()
+                .AddOptions()
+
+                .AddSingleton(new LoggerFactory().AddConsole(config.GetSection("Logging:Console")))
+                .AddLogging()
+
+        }
+    }
+}
